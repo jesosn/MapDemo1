3693c8b24b7a10b25104fdb6db8a9cf73cb8c372
hjj <646562345@qq.com>
2016-06-12 21:21:41 +0800

    first git pro

---
+++ app/src/main/java/com/example/administrator/mapdemo1/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.administrator.mapdemo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.poi.PoiSearch;
+
+
+public class MainActivity extends Activity {
+    MapView mMapView = null;
+    BaiduMap baiduMap = null;
+    PoiSearch poiSearch = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        baiduMap = mMapView.getMap();
+        baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        baiduMap.setTrafficEnabled(true);
+        poiSearch = PoiSearch.newInstance();
+
+
+        //定义Maker坐标点
+        LatLng point = new LatLng(39.963175, 116.400244);
+//构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.drawable.icon_marka);
+//构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions()
+                .position(point)
+                .icon(bitmap);
+//在地图上添加Marker，并显示
+        baiduMap.addOverlay(option);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+}
