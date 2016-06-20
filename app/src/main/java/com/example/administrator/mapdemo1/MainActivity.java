a4d69c3f89296c4daa33c2997d4b13eefaf0536b
hujunjie <646562345@qq.com>
2016-06-20 16:19:04 +0800

    update baiduSDK and so files and add show my location function.

--- app/src/main/java/com/example/administrator/mapdemo1/MainActivity.java
+++ app/src/main/java/com/example/administrator/mapdemo1/MainActivity.java
@@ -17,9 +17,15 @@ import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
 import com.baidu.mapapi.search.poi.PoiCitySearchOption;
 import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
 import com.baidu.mapapi.search.poi.PoiResult;
 import com.baidu.mapapi.search.poi.PoiSearch;
-
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.BDNotifyListener;//假如用到位置提醒功能，需要import该类
+import com.baidu.location.Poi;
 import java.util.List;
 
 
@@ -27,6 +33,10 @@ public class MainActivity extends Activity {
     MapView mMapView = null;
     BaiduMap baiduMap = null;
     PoiSearch poiSearch = null;
+
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,30 +51,61 @@ public class MainActivity extends Activity {
 //        baiduMap.setTrafficEnabled(true);
         poiSearch = PoiSearch.newInstance();
 
-        OnGetPoiSearchResultListener poiListener = new OnGetPoiSearchResultListener(){
-            public void onGetPoiResult(PoiResult result){
+        OnGetPoiSearchResultListener poiListener = new OnGetPoiSearchResultListener() {
+            public void onGetPoiResult(PoiResult result) {
                 //获取POI检索结果
                 List<PoiInfo> poiInfos = result.getAllPoi();
                 for (int i = 0; i < poiInfos.size(); i++) {
-                    Toast.makeText(MainActivity.this,poiInfos.get(i).name + poiInfos.get(i).address,Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, poiInfos.get(i).name + poiInfos.get(i).address, Toast.LENGTH_SHORT).show();
+                    showImageOnBaiduMap(poiInfos.get(i).location);
                 }
             }
-            public void onGetPoiDetailResult(PoiDetailResult result){
+
+            public void onGetPoiDetailResult(PoiDetailResult result) {
                 //获取Place详情页检索结果
+            }
+
+            @Override
+            public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+
             }
         };
         poiSearch.setOnGetPoiSearchResultListener(poiListener);
-
-
-
         //定义Maker坐标点
         LatLng point = new LatLng(39.963175, 116.400244);
 //构建Marker图标
+        showImageOnBaiduMap(point);
+
+
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+    public void showImageOnBaiduMap(LatLng latLng) {
         BitmapDescriptor bitmap = BitmapDescriptorFactory
                 .fromResource(R.drawable.icon_marka);
 //构建MarkerOption，用于在地图上添加Marker
         OverlayOptions option = new MarkerOptions()
-                .position(point)
+                .position(latLng)
                 .icon(bitmap);
 //在地图上添加Marker，并显示
         baiduMap.addOverlay(option);
@@ -72,7 +113,7 @@ public class MainActivity extends Activity {
 
     public void button_poi_click(View view) {
         poiSearch.searchInCity((new PoiCitySearchOption())
-                .city("南京")
+                .city("北京")
                 .keyword("美食")
                 .pageNum(5));
     }
@@ -86,16 +127,95 @@ public class MainActivity extends Activity {
         mMapView.onDestroy();
 
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+    }
+
+    public void showMyLocationOnClick(View view) {
+        initLocation();
+        mLocationClient.start();
+
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+//            Log.i("BaiduLocationApiDem", sb.toString());
+            //处理位置信息
+            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+            System.out.println(location.getLatitude()+"\t"+location.getLongitude());
+
+            showImageOnBaiduMap(latLng);
+        }
     }
 }
